63afb0426542968c89d4a95492ee25a7f5c744c0
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2021-03-18 12:18:22 -0400

    Update base.py

--- simple5ploit/modules/gather/internal/base.py
+++ simple5ploit/modules/gather/internal/base.py
@@ -2,21 +2,27 @@ from argparse import ArgumentParser
 
 class Gather:
 	def __init__(self):
-            self.info = {
-                "Author": "N/a",
-                "Author Repo": "N/a",
-                "Description": "N/a",
-                "Date Completed": "N/a"
+		self.info = {
+			"Author": "N/a",
+			"Author Repo": "N/a",
+			"Description": "N/a",
+			"Date Completed": "N/a"
 	    }
-            # For custom prompt, default is set below
-            self.prompt = "❯ "
-            self.args = {}
-	    self.cli_args = ArgumentParser()
-            self.pip_dependencies = []
-
+        # For custom prompt, default is set below
+        self.prompt = "❯ "
+		self.args = {}
+		self.cli_args = ArgumentParser()
+		self.pip_dependencies = []
+		
+		self.cli_args.add_argument(
+			"-rn", "--run",
+			action="store_true",
+			help="set programs args and run the module; don't enter interactive mode"
+		)
+	
 	def run(self):
-            """
-            If implemented, this function should contain
-            the scripts code to run against target
-            """
-            raise NotImplementedError
+		"""
+		If implemented, this function should contain
+		the scripts code to run against target
+		"""
+		raise NotImplementedError
