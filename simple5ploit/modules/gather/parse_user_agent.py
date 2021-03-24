6b9583471606443c87632b11edb267fe8f89de3a
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-03-24 17:24:21 -0400

    finishing user agent parser module

--- simple5ploit/modules/gather/parse_user_agent.py
+++ simple5ploit/modules/gather/parse_user_agent.py
@@ -1,12 +1,11 @@
 from .internal.base import Gather
 
-
 class UserAgent(Gather):
     def __init__(self):
-        self.prompt = "[UserAgentParser] >> "
+        self.prompt = "[UserAgentParser] :> "
         
         self.args = {
-            "user-agent":
+            "user_agent":
                 {
                     "description": "user agent string to parse",
                     "required": True
@@ -17,4 +16,21 @@ class UserAgent(Gather):
             self.__dict__[arg] = "N/a"
 
     def run(self):
-        pass
+        from json import loads
+        from requests import get
+
+        user_agent_string_api_url = "http://www.useragentstring.com/?uas="
+        resp = get(f"{user_agent_string_api_url}{self.__dict__['user_agent']}&getJSON=all").json()
+        
+        print("Results:")
+        print("\tAgentType \033[0;94m::\033[0m" + resp["agent_type"])
+        print("\tAgentName \033[0;94m::\033[0m " + resp["agent_name"])
+        print("\tAgentVersion \033[0;94m::\033[0m " + resp["agent_version"])
+        print("\tOSType \033[0;94m::\033[0m " + resp["os_type"])
+        print("\tOSName \033[0;94m::\033[0m " + resp["os_name"])
+        print("\tOSVersionName \033[0;94m::\033[0m " + resp["os_versionName"])
+        print("\tOSVersionNumber \033[0;94m::\033[0m " + resp["os_versionNumber"])
+        print("\tLinuxDistribution \033[0;94m::\033[0m " + resp["linux_distibution"])
+        
+
+
