e6886cefc1a6a331a6ec4b0ddefbb01dbf334fc2
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-03-24 11:28:58 -0400

    adding popular api functionality and history file

---
+++ simple5ploit/utils/shodan_connect.py
@@ -0,0 +1,13 @@
+from shodan import Shodan
+from config import ConfigParser
+
+class ShodanConnect:
+    def __init__(self):
+        self.config = ConfigParser()
+        self.config.read("../secrets.ini")
+
+    def search(self):
+        pass
+
+    def parse_search_results(self):
+        pass
---
+++ simple5ploit/utils/virus_total.py
@@ -0,0 +1,34 @@
+from vt import Client
+from configparser import ConfigParser
+
+class VT:
+    def __init__(self):
+        self.config = ConfigParser()
+        self.config.read("../secrets.ini")
+        api_key = self.config["virus_total"]["api_key"].strip()
+        if api_key:
+            self.api_key = api_key
+        else:
+            self.api_key = self.configure()
+        self.client = Client(self.api_key)
+
+    def configure(self):
+        print("[NOTE]::the following prompt will only appear"\
+                "\nthe first initial time simple5ploit is ran")
+        api_key = input("[++]::please enter your VirusTotal API key: ").strip()
+        self.config["virus_total"]["api_key"] = api_key
+        with open("../secrets.ini", "w") as fd:
+            self.config.write(fd)
+        return api_key
+
+    def scan_file(self):
+        pass
+
+    def scan_url(self):
+        pass
+
+    def get_file_info(self):
+        pass
+
+    def get_url_info(self):
+        pass
